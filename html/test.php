031d4237d5d67336c962f5f79696d592f7d71a11
alsrl1234 <alsrlrk123@naver.com>
2022-07-18 17:18:50 +0900

    1

---
+++ html/test.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "test111";
+        $string = "bravo my life";
+
+        echo $string;
+
+        $num = 100;
+        echo $num;
+    ?>
+    
+    <h1>나의 수학점수</h1>
+    <?php
+    if($num == 100){
+        echo "100점입니다."
+    }else
+    ?>
+
+    <script>
+        var text = "alslslsl"
+        document.write(text)
+    </script>
+</body>
+</html>
